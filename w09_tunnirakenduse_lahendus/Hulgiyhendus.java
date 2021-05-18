c9c12ac67b7d9b1c57bf09cb6808d1d963031279
Marcus-indrek Simmer <70900223+mxc2@users.noreply.github.com>
2021-05-19 01:43:24 +0300

    Delete Hulgiyhendus.java

--- w09_tunnirakenduse_lahendus/Hulgiyhendus.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-public abstract class Hulgiyhendus implements Voolujuht{
-    List<Voolujuht> hoidla=new ArrayList<Voolujuht>();
-    public void lisa(Voolujuht v){hoidla.add(v);}
-}
